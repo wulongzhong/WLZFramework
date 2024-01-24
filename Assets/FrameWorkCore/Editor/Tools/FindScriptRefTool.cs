d24d267c0642888d22f003597ca7fcfd97a9fb79
wulongzhong <1611136987@qq.com>
2024-01-24 09:43:10 +0800

    引用查询工具更新

--- Assets/FrameWorkCore/Editor/Tools/FindScriptRefTool.cs
+++ Assets/FrameWorkCore/Editor/Tools/FindScriptRefTool.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 
 public class FindScriptRefTool
 {
@@ -35,11 +36,113 @@ public class FindScriptRefTool
         }
 
         refPath.Sort();
-        foreach(var assetPath in refPath)
+        foreach (var assetPath in refPath)
         {
             Debug.Log(assetPath, AssetDatabase.LoadAssetAtPath<Object>(assetPath));
         }
 
         Debug.Log("查找资源引用结束");
+    }
+
+    [MenuItem("Assets/在当前场景中的引用", false, 1)]
+    static private void FindScriptRefInScene()
+    {
+        EditorSettings.serializationMode = SerializationMode.ForceText;
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        var instanceId = Selection.activeInstanceID;
+        var guid = AssetDatabase.AssetPathToGUID(path);
+
+
+        Debug.Log($"查找资源在场景中的{path}引用开始");
+
+        List<GameObject> listWaitCheckObj = new List<GameObject>();
+
+        EditorSceneManager.GetActiveScene().GetRootGameObjects(listWaitCheckObj);
+        while (listWaitCheckObj.Count > 0)
+        {
+            var lastObj = listWaitCheckObj[^1];
+            listWaitCheckObj.RemoveAt(listWaitCheckObj.Count - 1);
+            for (int i = 0; i < lastObj.transform.childCount; i++)
+            {
+                listWaitCheckObj.Add(lastObj.transform.GetChild(i).gameObject);
+            }
+            foreach (var bev in lastObj.GetComponents<UnityEngine.Component>())
+            {
+                if (bev == null)
+                {
+                    continue;
+                }
+                SerializedObject serialized = new SerializedObject(bev);
+                var iterator = serialized.GetIterator();
+                iterator.Reset();
+                while (iterator.NextVisible(true))
+                {
+                    if (iterator.propertyType == SerializedPropertyType.ObjectReference && iterator.objectReferenceInstanceIDValue == instanceId)
+                    {
+                        Debug.Log($"{GetGameObjectPath(lastObj)} : {iterator.propertyPath}", lastObj);
+                    }
+                }
+            }
+        }
+
+        Debug.Log("查找资源在场景中的引用结束");
+    }
+
+    [MenuItem("GameObject/在场景中的引用", false, 1)]
+    static private void FindGameObjectRefInScene()
+    {
+        EditorSettings.serializationMode = SerializationMode.ForceText;
+        var instanceId = Selection.activeInstanceID;
+
+        var listInstanceId = new List<int>();
+        listInstanceId.Add(Selection.activeInstanceID);
+        foreach (var bev in Selection.activeGameObject.GetComponents<UnityEngine.Component>())
+        {
+            listInstanceId.Add(bev.GetInstanceID());
+        }
+
+        Debug.Log($"查找资源在场景中的{Selection.activeGameObject.name}引用开始");
+
+        List<GameObject> listWaitCheckObj = new List<GameObject>();
+        EditorSceneManager.GetActiveScene().GetRootGameObjects(listWaitCheckObj);
+        while (listWaitCheckObj.Count > 0)
+        {
+            var lastObj = listWaitCheckObj[^1];
+            listWaitCheckObj.RemoveAt(listWaitCheckObj.Count - 1);
+            for (int i = 0; i < lastObj.transform.childCount; i++)
+            {
+                listWaitCheckObj.Add(lastObj.transform.GetChild(i).gameObject);
+            }
+            foreach (var bev in lastObj.GetComponents<UnityEngine.Component>())
+            {
+                if (bev == null)
+                {
+                    continue;
+                }
+                SerializedObject serialized = new SerializedObject(bev);
+                var iterator = serialized.GetIterator();
+                iterator.Reset();
+                while (iterator.NextVisible(true))
+                {
+                    if (iterator.propertyType == SerializedPropertyType.ObjectReference && listInstanceId.Contains(iterator.objectReferenceInstanceIDValue))
+                    {
+                        Debug.Log($"{GetGameObjectPath(lastObj)} : {iterator.propertyPath}", lastObj);
+                    }
+                }
+            }
+        }
+        Debug.Log("查找资源在场景中的引用结束");
+    }
+
+    private static string GetGameObjectPath(GameObject go)
+    {
+        StringBuilder goPath = new StringBuilder(go.name);
+        var parent = go.transform.parent;
+        while (parent != null)
+        {
+            goPath.Insert(0, "/").Insert(0, parent.gameObject.name);
+            parent = parent.parent;
+        }
+        return goPath.ToString();
     }
 }
